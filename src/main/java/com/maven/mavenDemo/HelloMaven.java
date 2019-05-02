e59c8ae7d66db92e83a19db3e644151746a5d70d
sandhyarenukarya <sandhya.yelagudige@gmail.com>
2019-05-02 14:55:54 -0700

    git commit for CI with Jenkins

---
+++ src/main/java/com/maven/mavenDemo/HelloMaven.java
@@ -0,0 +1,13 @@
+package com.maven.mavenDemo;
+
+public class HelloMaven 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Maven");
+		System.out.println("--------------------");
+		System.out.println("Doing CI with Jenkins from Githib commit");
+		
+	}
+
+}
